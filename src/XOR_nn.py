939f54b51d8621b2dcc47df89e84cbbe07ff3828
keshav nischal <keshavnischal@gmail.com>
2026-02-22 16:37:27 +0530

    pretrained weights to test

--- src/XOR_nn.py
+++ src/XOR_nn.py
@@ -8,7 +8,7 @@ class Neuron():
         for w, x in zip(self.weights, inputs):
             activation += w * x
         activation += self.bias
-        return activation   # needed for XOR
+        return activation  
 
 
 class Layer():
@@ -23,13 +23,15 @@ class Layer():
 x = [[0, 0], [0, 1], [1, 0], [1, 1]]
 
 # hidden layer
-neuron1 = Neuron([1, 1], 1)
-neuron2 = Neuron([1, 1], 1)
+neuron1 = Neuron([1, 1], -0.5)   # OR
+neuron2 = Neuron([1, 1], -1.5)   # AND
 hidden_layer = Layer([neuron1, neuron2])
 
 # output layer
-final_neuron = Neuron([1, 1], 0)
+final_neuron = Neuron([1, -2], -0.5)
 final_layer = Layer([final_neuron])
 
-print(final_layer.get_activation_vector(
-      hidden_layer.get_activation_vector([0, 1])))
+print(final_layer.get_activation_vector(hidden_layer.get_activation_vector([0, 0]))) #2
+print(final_layer.get_activation_vector(hidden_layer.get_activation_vector([0, 1]))) #1
+print(final_layer.get_activation_vector(hidden_layer.get_activation_vector([1, 0]))) #1
+print(final_layer.get_activation_vector(hidden_layer.get_activation_vector([1, 1]))) #0
